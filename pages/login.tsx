380968cffd91447c33676239daf1a1544e9d602b
Captain Price <murathoncu@gmail.com>
2023-01-11 19:27:46 +0300

    add formik and yup

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,39 @@
+import { useFormik } from "formik";
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
+import * as Yup from "yup";
+
+const loginSchema = Yup.object({
+  email: Yup.string()
+    .email("Please enter a valid email")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(8, "Minimum 8 character")
+    .max(16, "Maximum 16 character")
+    .required("Password is required"),
+});
+
+interface Inputs {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
+  const [login, setLogin] = useState(false);
+
+  const { handleSubmit, handleChange, values, errors } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: loginSchema,
+    onSubmit: (values, actions) => {
+      console.log(values);
+      actions.resetForm();
+    },
+  });
+
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
@@ -22,15 +54,50 @@ const Login = () => {
         height={150}
       />
 
-      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
-        <h1>Sign In</h1>
+      <form
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+        onClick={handleSubmit}>
+        <h1 className="text-4xl font-semibold">Sign In</h1>
         <div className="space-y-4">
-          <label>
-            <input type="email" placeholder="Email" className="input" />
+          <label className="inline-block w-full">
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light text-orange-500 ">
+                {errors.email}
+              </p>
+            )}
           </label>
-          <label>
-            <input type="password" placeholder="Password" className="input" />
+          <label className="inline-block w-full">
+            <input
+              type="password"
+              placeholder="Password"
+              className="input"
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light text-orange-500 ">
+                {errors.password}
+              </p>
+            )}
           </label>
+        </div>
+        <button className="w-full rounded bg-[#e50914] font-semibold py-3 ">
+          Sign In
+        </button>
+        <div className="text-[gray] ">
+          New to Netflix?{" "}
+          <button type="submit" className="text-white hover:underline">
+            Sign up now
+          </button>
         </div>
       </form>
     </div>
